43857a0eb4232d79c7033f5ba957a4653384f0c3
aditya-19-k <adityak19july@gmail.com>
2024-03-26 22:18:25 +0530

    working firebase date with map

--- app/src/main/java/com/example/vts/MapsActivity.java
+++ app/src/main/java/com/example/vts/MapsActivity.java
@@ -1,21 +1,36 @@
 package com.example.vts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Interpolator;
+import android.graphics.Point;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,34 +42,94 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import io.reactivex.rxjava3.annotations.NonNull;
+
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    public String latitudeAndLongitudeValue;
-    public String[] parts;
-    public double longitudeValue ;
-    //    public double longitudeValue = Double.valueOf(parts[1]);
-    public double latitudeValue ;
-//    public double latitudeValue = Double.valueOf(parts[0]);
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    public double latitudeValue;
+    public double longitudeValue;
+    private FusedLocationProviderClient fusedLocationClient;
+    public String latitude;
+    public String longitude;
+    public GoogleMap mMap;
 
+    // onCreate method is called when the activity is first created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.fragment_maps_activity);
 
-        //Read data from firebase
-        DatabaseReference reference;
-        reference = FirebaseDatabase.getInstance().getReference("location/latlng");
-        reference.addValueEventListener(new ValueEventListener() {
+
+//        // Get the SupportMapFragment and request notification
+//        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.maps_option, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.normal_map:
+//                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//                //do what you like
+//            default :
+//                return super.onOptionsItemSelected(item);
+//        }
+        Toast.makeText(this, String.valueOf(item.getItemId()), Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+
+
+    // This method is called when the map is ready to be used.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("location");
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                latitudeAndLongitudeValue = dataSnapshot.getValue(String.class);
-                parts = latitudeAndLongitudeValue.split(",");  //Split the string form comma (,)
-                Log.d("TAG", "Single Value: " + latitudeAndLongitudeValue);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    latitude = String.valueOf(dataSnapshot.child("latitude").getValue());
+                    longitude = String.valueOf(dataSnapshot.child("longitude").getValue());
+                    latitudeValue = Double.parseDouble(latitude);
+                    longitudeValue = Double.parseDouble(longitude);
+//                    Intent i = new Intent(MapsActivity.this, MapsActivity.class);
+//                    i.putExtra("latitude",latitude);
+//                    i.putExtra("longitude",longitude);
+//                    startActivity(i);
+                    SharedPreferences sharedPreferences = getSharedPreferences("location", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("latitude", latitude);
+                    editor.putString("longitude", longitude);
+                    editor.apply();
+
+//                        Toast.makeText(MapsActivity.this, latitude + "," + longitude, Toast.LENGTH_SHORT).show();
+                }
+
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
                 Toast.makeText(MapsActivity.this, databaseError.toString(), Toast.LENGTH_SHORT).show();
             }
 
@@ -62,65 +137,49 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         try {
 
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
+//            ##############################
 
-        }
-        catch (Exception exception){
-            Toast.makeText(this, String.valueOf(exception), Toast.LENGTH_SHORT).show();
-        }
 
-    }
+//            ################################
+//            Intent i = getIntent();
+//            latitudeValue = Double.parseDouble(i.getStringExtra("latitude"));
+//            longitudeValue = Double.parseDouble(i.getStringExtra("longitude"));
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+            SharedPreferences sharedPreferences = getSharedPreferences("location", MODE_PRIVATE);
+            latitude = sharedPreferences.getString("latitude", "");
+            longitude = sharedPreferences.getString("longitude", "");
+            Toast.makeText(MapsActivity.this, latitude + "," + longitude, Toast.LENGTH_SHORT).show();
 
-//            latitudeValue = 18.50254023911764;
-//            longitudeValue = 73.9394177840463;
-
-        try {
-            latitudeValue = Double.parseDouble(parts[0]);
-            longitudeValue = Double.parseDouble(parts[1]);
-            LatLng location = new LatLng(latitudeValue, longitudeValue);
-            mMap.addMarker(new
-
-                    MarkerOptions().position(location));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+            latitudeValue = Double.parseDouble(latitude);
+            longitudeValue = Double.parseDouble(longitude);
+            LatLng myPos = new LatLng(latitudeValue,longitudeValue);
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions()
+                    .visible(true)
+                    .position(myPos)
+                    .title("Your Vehical"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myPos));
             mMap.animateCamera(CameraUpdateFactory.zoomTo(16f));
 
+        }catch (Exception e){
+            latitudeValue = 18.50254023911764;
+            longitudeValue = 73.9394177840463;
+
+            LatLng myPos = new LatLng(latitudeValue,longitudeValue);
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myPos));
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions()
+                    .visible(true)
+                    .position(myPos)
+                    .title("Your Vehical"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myPos));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(16f));
+
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
         }
-        catch (Exception exception){
-            Toast.makeText((Context) this, String.valueOf(exception), Toast.LENGTH_SHORT).show();
-        }
+
 
     }
-
-//    public class RefreshMap {
-//
-//        private final Map<String, String> map = new HashMap<>();
-//
-//        public void main(String[] args) {
-//            Timer timer = new Timer();
-//            timer.schedule(new TimerTask() {
-//                @Override
-//                public void run() {
-//                    // Refresh the map
-//                    map.put("key", "AIzaSyBHKRg9MD0B6AWDLF64JBBzG8fWMsSF5TI");
-//                }
-//            }, 1000);
-//        }
-//    }
 
 }
